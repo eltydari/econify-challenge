140a4346c1e8db78508cff84b3333e33afb6ca74
George Ge <george.jiaqi.ge@gmail.com>
2020-04-27 06:22:02 -0400

    Added initial mutators

--- src/schema/index.js
+++ src/schema/index.js
@@ -1,5 +1,10 @@
-import { GraphQLObjectType, GraphQLSchema } from 'graphql';
-import { GraphQLString } from 'graphql';
+import { 
+    GraphQLObjectType, 
+    GraphQLSchema,
+    GraphQLNonNull,
+    GraphQLString,
+    GraphQLBoolean
+} from 'graphql';
 import OrganizationType from './organization';
 import EventType from './event';
 import LocationType from './location';
@@ -33,6 +38,68 @@ const QueryType = new GraphQLObjectType({
                 name: { type: GraphQLString }
             },
             resolve: getEvent
+        }
+    })
+});
+
+const MutationType = new GraphQLObjectType({
+    name: 'Mutation',
+    description: 'Mutation of orgs and events',
+    fields: () => ({
+        addEvent: {
+            type: EventType,
+            args: {
+                name: { type: GraphQLNonNull(GraphQLString) },
+                date: { type: GraphQLNonNull(GraphQLString) },
+                time: { type: GraphQLNonNull(GraphQLString) },
+                am: { type: GraphQLNonNull(GraphQLBoolean) },
+                organization: { type: GraphQLNonNull(GraphQLString) },
+                description: { type: GraphQLString }
+            }
+        },
+        addLocation: {
+            type: LocationType,
+            args: {
+                name: { type: GraphQLNonNull(GraphQLString) },
+                address: { type: GraphQLNonNull(GraphQLString) },
+                organization: { type: GraphQLNonNull(GraphQLString) },
+            },
+            resolve: () => {},
+        },
+        updateEvent: {
+            type: EventType,
+            args: {
+                name: { type: GraphQLNonNull(GraphQLString) },
+                date: { type: GraphQLString },
+                time: { type: GraphQLString },
+                am: { type: GraphQLBoolean },
+                organization: { type: GraphQLString },
+                description: { type: GraphQLString }
+            },
+            resolve: () => {}
+        },
+        updateLocation: {
+            type: LocationType,
+            args: {
+                name: { type: GraphQLNonNull(GraphQLString) },
+                address: { type: GraphQLString },
+                organization: { type: GraphQLString },
+            },
+            resolve: () => {}
+        },
+        deleteEvent: {
+            type: EventType,
+            args: {
+                name: { type: GraphQLNonNull(GraphQLString) },
+            },
+            resolve: () => {}
+        },
+        deleteLocation: {
+            type: LocationType,
+            args: {
+                name: { type: GraphQLNonNull(GraphQLString) },
+            },
+            resolve: () => {}
         }
     })
 });
